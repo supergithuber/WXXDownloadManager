f6e0737f7d3f65a8ca41e8a7341e5a251562523f
sivanWu <>
2018-05-24 10:50:09 +0800

    initial commit

---
+++ WXXDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXXDownloadManager
+//
+//  Created by HFY on 2018/5/24.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXXDownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXXDownloadManager
+//
+//  Created by HFY on 2018/5/24.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
